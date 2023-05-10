e097f91eabbd0032ecac40cb999d1714f9d433df
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-10 17:32:57 -0300

    add initialize bash file and run stellar quickstart image #2

--- quickstart.sh
+++ quickstart.sh
@@ -1,3 +1,4 @@
+echo "1. Running a soroban-precview docker container"
 previewVersion="8"
 
 echo "Searching for a previous soroban-preview docker container"
@@ -20,3 +21,17 @@ docker run --volume  ${currentDir}:/workspace \
            --ipc=host \
            --network soroban-network \
            esteblock/soroban-preview:8
+
+
+echo "2. Running a stellar quickstart docker container"
+
+# Run the stellar quickstart image
+docker run --rm -ti \
+  --platform linux/amd64 \
+  --name stellar \
+  --network soroban-network \
+  -p 8000:8000 \
+  stellar/quickstart:soroban-dev@sha256:81c23da078c90d0ba220f8fc93414d0ea44608adc616988930529c58df278739 \
+  $ARGS \
+  --enable-soroban-rpc \
+  --protocol-version 20 
