35e97c84e8ae0fba1490293db0585c40ad5776e5
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-10 17:32:57 -0300

    add initialize bash file and run stellar quickstart image #2

---
+++ initialize.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+set -e
+
+NETWORK="$1"
+
+# If soroban-cli is called inside the soroban-preview docker containter,
+# it can call the stellar standalone container just using its name "stellar"
+# if [[ "$IS_USING_DOCKER" == "true" ]]; then
+  SOROBAN_RPC_HOST="http://stellar:8000"
+# else
+  # SOROBAN_RPC_HOST="http://localhost:8000"
+# fi
+
+SOROBAN_RPC_URL="$SOROBAN_RPC_HOST/soroban/rpc"
+
+case "$1" in
+standalone)
+  echo "Using standalone network"
+  SOROBAN_NETWORK_PASSPHRASE="Standalone Network ; February 2017"
+  FRIENDBOT_URL="$SOROBAN_RPC_HOST/friendbot"
+  ;;
+futurenet)
+  echo "Using Futurenet network"
+  SOROBAN_NETWORK_PASSPHRASE="Test SDF Future Network ; October 2022"
+  FRIENDBOT_URL="https://friendbot-futurenet.stellar.org/"
+  ;;
+*)
+  echo "Usage: $0 standalone|futurenet"
+  exit 1
+  ;;
+esac
+
+#if !(soroban config network ls | grep "$NETWORK" 2>&1 >/dev/null); then
+# Always set a net configuration 
+  echo Add the $NETWORK network to cli client
+  soroban config network add "$NETWORK" \
+    --rpc-url "$SOROBAN_RPC_URL" \
+    --network-passphrase "$SOROBAN_NETWORK_PASSPHRASE"
+#fi
+
+if !(soroban config identity ls | grep token-admin 2>&1 >/dev/null); then
+  echo Create the token-admin identity
+  soroban config identity generate token-admin
+fi
+TOKEN_ADMIN_SECRET="$(soroban config identity show token-admin)"
+TOKEN_ADMIN_ADDRESS="$(soroban config identity address token-admin)"
+
+# TODO: Remove this once we can use `soroban config identity` from webpack.
+echo "$TOKEN_ADMIN_SECRET" > .soroban/token_admin_secret
+echo "$TOKEN_ADMIN_ADDRESS" > .soroban/token_admin_address
+
+# This will fail if the account already exists, but it'll still be fine.
+echo Fund token-admin account from friendbot
+curl  -X POST "$FRIENDBOT_URL?addr=$TOKEN_ADMIN_ADDRESS" >/dev/null
+
+ARGS="--network $NETWORK --source token-admin"
+
+echo Wrap two Stellar asset
+mkdir -p .soroban
+TOKEN_0_ID=$(soroban lab token wrap $ARGS --asset "TOKEN0:$TOKEN_ADMIN_ADDRESS")
+TOKEN_1_ID=$(soroban lab token wrap $ARGS --asset "TOKEN1:$TOKEN_ADMIN_ADDRESS")
+
+echo "token_0 was wrapped succesfully with TOKEN_0_ID: $TOKEN_0_ID"
+echo "token_1 was wrapped succesfully with TOKEN_1_ID: $TOKEN_1_ID"
+
+
+echo -n "$TOKEN_0_ID" > .soroban/token_0_id
+echo -n "$TOKEN_0_ID" > .soroban/token_1_id
+
+# echo Build the crowdfund contract
+# make build
+
+# echo Deploy the crowdfund contract
+# CROWDFUND_ID="$(
+#   soroban contract deploy $ARGS \
+#     --wasm target/wasm32-unknown-unknown/release/soroban_crowdfund_contract.wasm
+# )"
+# echo "$CROWDFUND_ID" > .soroban/crowdfund_id
+
+# echo "Contract deployed succesfully with ID: $CROWDFUND_ID"
+
+# echo "Initialize the crowdfund contract"
+# deadline="$(($(date +"%s") + 86400))"
+# soroban contract invoke \
+#   $ARGS \
+#   --wasm target/wasm32-unknown-unknown/release/soroban_crowdfund_contract.wasm \
+#   --id "$CROWDFUND_ID" \
+#   -- \
+#   initialize \
+#   --recipient "$TOKEN_ADMIN_ADDRESS" \
+#   --deadline "$deadline" \
+#   --target_amount "1000000000" \
+#   --token "$TOKEN_ID"
+
+# echo "Done"
