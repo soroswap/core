73199e5a116a2bc007afdf968640dfcf07667fef
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-11 20:40:28 -0300

    fix SDK v0.7.0

--- initialize.sh
+++ initialize.sh
@@ -64,13 +64,13 @@ mkdir -p .soroban
 echo "--"
 echo "--"
 echo "Wrapping TOKENA:$TOKEN_ADMIN_ADDRESS"
-TOKEN_A_ID=$(soroban lab token wrap $ARGS --asset "TOKENA:$TOKEN_ADMIN_ADDRESS")
+TOKEN_A_ID=$(soroban lab token wrap $ARGS --asset "FTTTA:$TOKEN_ADMIN_ADDRESS")
 echo "token_0 was wrapped succesfully with TOKEN_A_ID: $TOKEN_A_ID"
 echo "--"
 echo "--"
 
 echo "Wrapping TOKENB:$TOKEN_ADMIN_ADDRESS"
-TOKEN_B_ID=$(soroban lab token wrap $ARGS --asset "TOKENB:$TOKEN_ADMIN_ADDRESS")
+TOKEN_B_ID=$(soroban lab token wrap $ARGS --asset "FTTTB:$TOKEN_ADMIN_ADDRESS")
 echo "token_1 was wrapped succesfully with TOKEN_B_ID: $TOKEN_B_ID"
 echo "--"
 echo "--"
@@ -81,17 +81,30 @@ echo -n "$TOKEN_0_ID" > .soroban/token_1_id
 echo Build the SoroswapPair and SoroswapFactory contract
 make build
 FACTORY_WASM="factory/target/wasm32-unknown-unknown/release/soroswap_factory_contract.wasm"
-PAIR_WASM="factory/target/wasm32-unknown-unknown/release/pair_factory_contract.wasm"
+PAIR_WASM="factory/target/wasm32-unknown-unknown/release/soroswap_pair_contract.wasm"
+echo "--"
+echo "--"
+echo Install the Pair contract WASM
+echo Install a WASM file to the ledger without creating a contract instance
 
-echo Deploy the SoroswapFactory contract
+PAIR_WASM_HASH="$(
+soroban contract deploy $ARGS \
+  --wasm $PAIR_WASM
+)"
+echo "$PAIR_WASM_HASH" > .soroban/pair_wasm_hash
+echo "SoroswapPair deployed succesfully with PAIR_WASM_HASH: $PAIR_WASM_HASH"
+echo "--"
+echo "--"
 
+echo Deploy the Factory contract
 FACTORY_ID="$(
   soroban contract deploy $ARGS \
     --wasm $FACTORY_WASM
 )"
 echo "$FACTORY_ID" > .soroban/factory_id
-
 echo "SoroswapFactory deployed succesfully with FACTORY_ID: $FACTORY_ID"
+echo "--"
+echo "--"
 
 echo "Initialize the SoroswapFactory contract"
 soroban contract invoke \
@@ -101,8 +114,20 @@ soroban contract invoke \
   -- \
   initialize \
   --setter "$TOKEN_ADMIN_ADDRESS" \
-  --pair_wasm_hash "$PAIR_WASM" 
+  --pair_wasm_hash "$PAIR_WASM_HASH" 
 
-# echo "Done"
+echo "--"
+echo "--"
 
- setter: Address, pair_wasm_hash: BytesN<32>
+echo "Create a pair using the SoroswapFactory contract, token_a and token_b"
+soroban contract invoke \
+  $ARGS \
+  --wasm $FACTORY_WASM \
+  --id $FACTORY_ID \
+  -- \
+  create_pair \
+  --token_a "$TOKEN_A_ID" \
+  --token_b "$TOKEN_B_ID" 
+
+echo "--"
+echo "--"
