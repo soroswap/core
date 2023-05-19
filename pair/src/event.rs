0b49a4c4336cdc5684f5676d6d00af05f7fa27f6
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-19 14:55:32 -0300

    feat(pair): added events #3

---
+++ pair/src/event.rs
@@ -0,0 +1,54 @@
+use soroban_sdk::{Env, Symbol, BytesN, Address};
+
+//  event Mint(address indexed sender, uint amount0, uint amount1);
+pub(crate) fn mint(e: &Env, sender: Address, amount_0: u32, amount_1: u32) {
+    let topics = (Symbol::new(e, "mint"), sender, amount_0);
+    e.events().publish(topics, amount_1);
+}
+
+// event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+pub(crate) fn burn(e: &Env, sender: Address, amount_0: u32, amount_1: u32, to: Address) {
+    let topics = (Symbol::new(e, "burn"), sender, amount_0, amount_1);
+    e.events().publish(topics, to);
+}
+
+/*
+event Swap(
+     address indexed sender,
+     uint amount0In,
+     uint amount1In,
+     uint amount0Out,
+     uint amount1Out,
+     address indexed to
+ );
+*/
+
+pub(crate) fn swap(
+    e: &Env,
+    sender: Address,
+    amount_0_in: u32,
+    amount_1_in: u32,
+    amount_0_out: u32,
+    amount_1_out: u32,
+    to: Address,
+) {
+    let topics = (
+        Symbol::new(e, "swap"),
+        sender,
+        amount_0_in,
+        amount_1_in,
+        amount_0_out,
+        amount_1_out
+    );
+    e.events().publish(topics, to);
+}
+
+
+
+// event Sync(uint112 reserve0, uint112 reserve1);
+
+// TODO: Implement when implementing sync function
+// pub(crate) fn sync(e: &Env, reserve_0: u32, reserve_1: u32) {
+//     let topics = (Symbol::new(e, "sync"), reserve_0);
+//     e.events().publish(topics, reserve_1);
+
