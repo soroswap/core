08fef4b1c4f28dfcdddce8282853ba26f6c7376b
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-25 15:26:47 -0300

    Added events

--- pair/src/event.rs
+++ pair/src/event.rs
@@ -4,13 +4,13 @@ const PAIR: Symbol = Symbol::short("PAIR");
 
 
 pub(crate) fn deposit(e: &Env, sender: Address, amount_0: i128, amount_1: i128) {
-    let topics = (PAIR, Symbol::new(e, "deposit"));
-    e.events().publish(topics, (sender, amount_0, amount_1));
+    let topics = (PAIR, Symbol::new(e, "deposit"), sender);
+    e.events().publish(topics, (amount_0, amount_1));
 }
  
 pub(crate) fn withdraw(e: &Env, sender: Address, amount_0: i128, amount_1: i128, to: Address) {
-    let topics = (Symbol::new(e, "withdraw"), sender, amount_0, amount_1);
-    e.events().publish(topics, to);
+    let topics = (PAIR, Symbol::new(e, "withdraw"), sender);
+    e.events().publish(topics, (amount_0, amount_1, to));
 }
 
 /*
@@ -24,32 +24,24 @@ event Swap(
  );
 */
 
-// pub(crate) fn swap(
-//     e: &Env,
-//     sender: Address,
-//     amount_0_in: i128,
-//     // amount_1_in: i128,
-//     //amount_0_out: i128,
-//     amount_1_out: i128,
-//     to: Address,
-// ) {
-//     let topics = (
-//         Symbol::new(e, "swap"),
-//         sender,
-//         //amount_0_in,
-//         // amount_1_in,
-//         //amount_0_out,
-//         amount_1_out
-//     );
-//     e.events().publish(topics, (amount_0_in, amount_0_in, amount_0_in,  to));
-// }
+pub(crate) fn swap(
+    e: &Env,
+    sender: Address,
+    amount_0_in: i128,
+    amount_1_in: i128,
+    amount_0_out: i128,
+    amount_1_out: i128,
+    to: Address,
+) {
+    let topics = (PAIR, Symbol::new(e, "swap"), sender);
+    e.events().publish(topics, (amount_0_in, amount_1_in, amount_0_out,amount_1_out,  to));
+}
 
 
 
 // event Sync(uint112 reserve0, uint112 reserve1);
-
-// TODO: Implement when implementing sync function
-// pub(crate) fn sync(e: &Env, reserve_0: u32, reserve_1: u32) {
-//     let topics = (Symbol::new(e, "sync"), reserve_0);
-//     e.events().publish(topics, reserve_1);
+pub(crate) fn sync(e: &Env, reserve_0: u32, reserve_1: u32) {
+    let topics = (PAIR, Symbol::new(e, "sync"));
+    e.events().publish(topics, (reserve_0, reserve_1));
+}
 
