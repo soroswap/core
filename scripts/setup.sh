444781240413bba29c18786c73fbce4a2a31b951
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-06-28 18:38:59 -0300

    separate setup

---
+++ scripts/setup.sh
@@ -0,0 +1,50 @@
+# Run script from project root
+
+set -e
+
+NETWORK="$1"
+
+
+# If soroban-cli is called inside the soroban-preview docker containter,
+# it can call the stellar standalone container just using its name "stellar"
+SOROBAN_RPC_HOST="http://stellar:8000"
+
+SOROBAN_RPC_URL="$SOROBAN_RPC_HOST/soroban/rpc"
+
+case "$1" in
+standalone)
+  echo "Using standalone network"
+  SOROBAN_NETWORK_PASSPHRASE="Standalone Network ; February 2017"
+  FRIENDBOT_URL="$SOROBAN_RPC_HOST/friendbot"
+  ;;
+futurenet)
+  echo "Using Futurenet network"
+  SOROBAN_NETWORK_PASSPHRASE="Test SDF Future Network ; October 2022"
+  FRIENDBOT_URL="https://friendbot-futurenet.stellar.org/"
+  ;;
+*)
+  echo "Usage: $0 standalone|futurenet"
+  exit 1
+  ;;
+esac
+
+# Always set a net configuration 
+echo Add the $NETWORK network to cli client
+soroban config network add "$NETWORK" \
+  --rpc-url "$SOROBAN_RPC_URL" \
+  --network-passphrase "$SOROBAN_NETWORK_PASSPHRASE"
+
+if !(soroban config identity ls | grep token-admin 2>&1 >/dev/null); then
+  echo Create the token-admin identity
+  soroban config identity generate token-admin
+fi
+TOKEN_ADMIN_SECRET="$(soroban config identity show token-admin)"
+TOKEN_ADMIN_ADDRESS="$(soroban config identity address token-admin)"
+
+echo "We are using the following TOKEN_ADMIN_ADDRESS: $TOKEN_ADMIN_ADDRESS"
+
+
+# This will fail if the account already exists, but it'll still be fine.
+echo Fund token-admin account from friendbot
+echo This will fail if the account already exists, but it\' still be fine.
+curl  -X POST "$FRIENDBOT_URL?addr=$TOKEN_ADMIN_ADDRESS"
