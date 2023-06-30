34bd9eab981f533420e4dbd6ea41e57eecddd7d7
joaquinsoza <joaquinsozag@gmail.com>
2023-06-30 15:53:57 -0400

    feat: serving factory and admin keys, added tokens indexed

--- api/server.js
+++ api/server.js
@@ -16,6 +16,29 @@ app.get('/api/tokens', (req, res) => {
   res.status(404).send({ 'error': 'file not found'})
 });
 
+app.get('/api/factory', (req, res) => {
+  const factoryFile = '/workspace/.soroban/factory.json' 
+
+  if (fs.existsSync(factoryFile)) {
+    return res.sendFile(factoryFile);
+  }
+
+  res.status(404).send({ 'error': 'file not found'})
+});
+
+app.get('/api/keys', (req, res) => {
+  const addressFile = '/workspace/.soroban/token_admin_address' 
+  const secretFile = '/workspace/.soroban/token_admin_secret' 
+  
+  if (fs.existsSync(addressFile) && fs.existsSync(secretFile)) {
+    const address = fs.readFileSync(addressFile, 'utf8').trim();
+    const secret = fs.readFileSync(secretFile, 'utf8').trim();
+    return res.json({ admin_public: address, admin_secret: secret });
+  }
+
+  res.status(404).send({ 'error': 'file not found'})
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
