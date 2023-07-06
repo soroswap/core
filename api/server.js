9116b463a4deaa81ef8453cb21b5a7c80f145fb0
josefredes <jose.fredes@mayor.cl>
2023-07-05 23:48:30 -0400

    feat: add endpoint to serve pairs

--- api/server.js
+++ api/server.js
@@ -7,33 +7,43 @@ const port = 8010;
 app.use(cors());
 
 app.get('/api/tokens', (req, res) => {
-  const tokensFile = '/workspace/.soroban/tokens.json' 
+  const tokensFile = '/workspace/.soroban/tokens.json';
 
   if (fs.existsSync(tokensFile)) {
     return res.sendFile(tokensFile);
   }
 
-  res.status(404).send({ 'error': 'file not found'})
+  res.status(404).send({ error: 'file not found' });
 });
 
 app.get('/api/factory', (req, res) => {
-  const factoryFile = '/workspace/.soroban/factory.json' 
+  const factoryFile = '/workspace/.soroban/factory.json';
 
   if (fs.existsSync(factoryFile)) {
     return res.sendFile(factoryFile);
   }
 
-  res.status(404).send({ 'error': 'file not found'})
+  res.status(404).send({ error: 'file not found' });
 });
 
 app.get('/api/keys', (req, res) => {
-  const keysFile = '/workspace/.soroban/token_admin_keys.json' 
+  const keysFile = '/workspace/.soroban/token_admin_keys.json';
 
   if (fs.existsSync(keysFile)) {
     return res.sendFile(keysFile);
   }
 
-  res.status(404).send({ 'error': 'file not found'})
+  res.status(404).send({ error: 'file not found' });
+});
+
+app.get('/api/pairs', (req, res) => {
+  const tokensFile = '/workspace/.soroban/temp_pairs.json';
+
+  if (fs.existsSync(tokensFile)) {
+    return res.sendFile(tokensFile);
+  }
+
+  res.status(404).send({ error: 'file not found' });
 });
 
 app.listen(port, () => {
