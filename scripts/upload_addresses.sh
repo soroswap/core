684762c202b56b30cac617333960e45e3a4c06a5
Contract Addresses Updater <you@example.com>
2023-07-07 23:39:22 +0000

    feat: script that moves tokens.json, pairs.json and factory.json to public/ and commit them to the repo, folder to serve it on Vercel

---
+++ scripts/upload_addresses.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Current date
+DATE=$(date '+%Y-%m-%d')
+
+# Source and target directories
+SRC_DIR="/workspace/.soroban"
+TARGET_DIR="/workspace/public"
+
+# File names
+declare -a FILES=("tokens.json" "pairs.json" "factory.json")
+
+# Copy files from .soroban to public/
+for FILE in "${FILES[@]}"
+do
+    cp "$SRC_DIR/$FILE" "$TARGET_DIR"
+    git add "$TARGET_DIR/$FILE" 
+done
+
+git config --global --add safe.directory /workspace
+git config --global user.email "you@example.com"
+git config --global user.name "Contract Addresses Updater"
+
+## Git related commands
+
+git commit -m "data: $DATE updated contract addresses"
+
+git push origin main
