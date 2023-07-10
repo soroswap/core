1353962f7a84a21f5a775a3bd989b7756aba47c8
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-07-10 00:15:24 -0300

    Also publish token_admin_keys

--- scripts/upload_addresses.sh
+++ scripts/upload_addresses.sh
@@ -8,7 +8,7 @@ SRC_DIR="/workspace/.soroban"
 TARGET_DIR="/workspace/public"
 
 # File names
-declare -a FILES=("tokens.json" "pairs.json" "factory.json")
+declare -a FILES=("tokens.json" "pairs.json" "factory.json" "token_admin_keys.json")
 git config --global --add safe.directory /workspace
 
 # Copy files from .soroban to public/
