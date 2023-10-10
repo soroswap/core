1f93b8c6031842d614f0185f7f8b6709174128e6
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-10-10 03:00:02 +0200

    remove unused datakey

--- contracts/library/src/lib.rs
+++ contracts/library/src/lib.rs
@@ -4,7 +4,7 @@ mod test;
 
 use soroban_sdk::{
     contract,
-    contractimpl, Address, BytesN, ConversionError, Env, Val, TryFromVal,
+    contractimpl, Address, BytesN, Env,
     xdr::ToXdr, Vec, Bytes,
 };
 
@@ -15,26 +15,6 @@ mod pair {
     );
 }
 
-#[derive(Clone, Copy)]
-#[repr(u32)]
-
-pub enum DataKey {
-    FeeTo = 0,        // address public feeTo;
-    FeeToSetter = 1,  // address public feeToSetter;
-    AllPairs = 2,     //  address[] public allPairs;
-    PairsMapping = 3, // Map of pairs
-    PairWasmHash = 4,
-    FeesEnabled = 5, // bool is taking fees?
-}
-
-
-impl TryFromVal<Env, DataKey> for Val {
-    type Error = ConversionError;
-
-    fn try_from_val(_env: &Env, v: &DataKey) -> Result<Self, Self::Error> {
-        Ok((*v as u32).into())
-    }
-}
 
 fn pair_salt(e: &Env, token_a: Address, token_b: Address) -> BytesN<32> {
     let mut salt = Bytes::new(e);
