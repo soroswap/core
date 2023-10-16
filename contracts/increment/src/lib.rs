c5439bd5991206fa3b39527a82b450e7aae74afe
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-10-16 15:12:35 -0300

    increment

--- contracts/increment/src/lib.rs
+++ contracts/increment/src/lib.rs
@@ -1,20 +1,21 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl};
+//use soroban_sdk::{contract, contractimpl};
+use soroban_sdk;
 
 
 pub fn my_increment(count: u32) -> u32 {
     count + 1
 }
 
-#[contract]
-pub struct IncrementContract;
+// #[contract]
+// pub struct IncrementContract;
 
-#[contractimpl]
-impl IncrementContract {
-    /// Increment increments an internal counter, and returns the value.
-    pub fn increment(count: u32) -> u32 {
-        my_increment(count)
-    }
-}
+// #[contractimpl]
+// impl IncrementContract {
+//     /// Increment increments an internal counter, and returns the value.
+//     pub fn increment(count: u32) -> u32 {
+//         my_increment(count)
+//     }
+// }
 
-mod test;
+// mod test;
