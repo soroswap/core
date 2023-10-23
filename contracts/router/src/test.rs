b8be23e61b72b843895058c7c98113d3b5ce08f5
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-10-23 16:13:39 -0300

    tokens, admin and user in test context

--- contracts/router/src/test.rs
+++ contracts/router/src/test.rs
@@ -1,16 +1,31 @@
 #![cfg(test)]
 extern crate std;
 use crate::{SoroswapRouter, SoroswapRouterClient};
-use soroban_sdk::{Env};
+use soroban_sdk::{Env, Address, testutils::Address as _};
 
+mod token {
+    soroban_sdk::contractimport!(file = "../token/soroban_token_contract.wasm");
+    pub type TokenClient<'a> = Client<'a>;
+}
+
+use token::TokenClient;
 
 fn create_soroswap_router_contract<'a>(e: &Env) -> SoroswapRouterClient<'a> {
     SoroswapRouterClient::new(e, &e.register_contract(None, SoroswapRouter {}))
 }
 
+fn create_token_contract<'a>(e: &Env, admin: & Address) -> TokenClient<'a> {
+    TokenClient::new(&e, &e.register_stellar_asset_contract(admin.clone()))
+}
+
+
 struct SoroswapRouterTest<'a> {
     env: Env,
     contract: SoroswapRouterClient<'a>,
+    token_0: TokenClient<'a>,
+    token_1: TokenClient<'a>,
+    admin: Address,
+    user: Address,
 }
 
 impl<'a> SoroswapRouterTest<'a> {
@@ -20,9 +35,24 @@ impl<'a> SoroswapRouterTest<'a> {
         env.mock_all_auths();
         let contract = create_soroswap_router_contract(&env);
 
+        let admin = Address::random(&env);
+        let user = Address::random(&env);
+
+        let mut token_0 = create_token_contract(&env, &admin);
+        let mut token_1 = create_token_contract(&env, &admin);
+        if &token_1.address.contract_id() < &token_0.address.contract_id() {
+            std::mem::swap(&mut token_0, &mut token_1);
+        }
+        token_0.mint(&user, &10000);
+        token_1.mint(&user, &10000);
+
         SoroswapRouterTest {
             env,
             contract,
+            token_0,
+            token_1,
+            admin,
+            user
         }
     }
 }
