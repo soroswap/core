aaed75430780305c7b0f1320dd608bddc88b0c95
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-10-23 15:48:51 -0300

    mode add_liquidity test to external mod file

--- contracts/router/src/test.rs
+++ contracts/router/src/test.rs
@@ -7,12 +7,7 @@ use soroban_sdk::{Env,
     Address,
     testutils::{
         Address as _, 
-        MockAuth,
-        MockAuthInvoke,
-        Ledger,
-    },
-    vec,
-    IntoVal};
+    }};
 
 
 fn create_soroswap_router_contract<'a>(e: &Env) -> SoroswapRouterClient<'a> {
@@ -65,85 +60,4 @@ fn test_initialize_twice() {
     test.contract.initialize(&factory_another);
 }
 
-#[test]
-#[should_panic(expected = "Unauthorized function call for address")]
-fn test_add_liquidity_not_authorized() {
-    let test = SoroswapRouterTest::setup();
-    let alice = Address::random(&test.env);
-    let bob = Address::random(&test.env);
-    // alice is not equal to bob
-    assert_ne!(alice, bob);
-    let token_a = Address::random(&test.env);
-    let token_b = Address::random(&test.env);
-
-    /*
-        Here we test the add_liquidity function "to.require_auth();" requirement
-        So if alice calls the function but sets "bob" in the "to" argument, this should fail
-    */
-    test.contract
-        .mock_auths(&[MockAuth {
-            address: &alice,
-            invoke: &MockAuthInvoke {
-                contract: &test.contract.address,
-                fn_name: "add_liquidity",
-                args: vec![
-                    &test.env,
-                    token_a.into_val(&test.env), //     token_a: Address,
-                    token_b.into_val(&test.env), //     token_b: Address,
-                    0.into_val(&test.env), //     amount_a_desired: i128,
-                    0.into_val(&test.env), //     amount_b_desired: i128,
-                    0.into_val(&test.env), //     amount_a_min: i128,
-                    0.into_val(&test.env) , //     amount_b_min: i128,
-                    (&bob,).into_val(&test.env), //     to: Address,
-                    0.into_val(&test.env)//     deadline: u64,
-                    ],
-                sub_invokes: &[],
-            },
-        }])
-        .add_liquidity(
-            &token_a, //     token_a: Address,
-            &token_b, //     token_b: Address,
-            &0, //     amount_a_desired: i128,
-            &0, //     amount_b_desired: i128,
-            &0, //     amount_a_min: i128,
-            &0 , //     amount_b_min: i128,
-            &bob, //     to: Address,
-            &0//     deadline: u64,
-        );
-
-}
-
-#[test]
-#[should_panic(expected = "SoroswapRouter: expired")]
-fn test_add_liquidity_deadline_expired() {
-    let test = SoroswapRouterTest::setup();
-    let alice = Address::random(&test.env);
-    let bob = Address::random(&test.env);
-    // alice is not equal to bob
-    assert_ne!(alice, bob);
-    let token_a = Address::random(&test.env);
-    let token_b = Address::random(&test.env);
-
-    let ledger_timestamp = 100;
-    let desired_deadline = 90;
-
-    assert!(desired_deadline < ledger_timestamp);
-
-    test.env.ledger().with_mut(|li| {
-        li.timestamp = 100;
-    });
-
-    // /*
-    //     Here we test the case when deadline has passed
-    //  */
-    test.contract.add_liquidity(
-        &token_a, //     token_a: Address,
-        &token_b, //     token_b: Address,
-        &0, //     amount_a_desired: i128,
-        &0, //     amount_b_desired: i128,
-        &0, //     amount_a_min: i128,
-        &0 , //     amount_b_min: i128,
-        &bob, //     to: Address,
-        &desired_deadline//     deadline: u64,
-    );
-}
+pub mod add_liquidity;
