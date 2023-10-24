bce5859096582177de3b14480d29296fa5cd9a9c
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-10-24 14:31:11 -0300

    test_remove_liquidity

--- contracts/router/src/test/remove_liquidity.rs
+++ contracts/router/src/test/remove_liquidity.rs
@@ -126,3 +126,71 @@ fn test_remove_liquidity_pair_does_not_exist() {
         &desired_deadline//     deadline: u64,
     );
 }
+
+fn add_liquidity(test: &SoroswapRouterTest){
+    let ledger_timestamp = 100;
+    let desired_deadline = 1000;
+    assert!(desired_deadline > ledger_timestamp);
+    test.env.ledger().with_mut(|li| {
+        li.timestamp = ledger_timestamp;
+    });
+
+    let amount_0: i128 = 1_000_000_000_000_000_000;
+    let amount_1: i128 = 4_000_000_000_000_000_000;
+
+    test.contract.add_liquidity(
+        &test.token_0.address, //     token_a: Address,
+        &test.token_1.address, //     token_b: Address,
+        &amount_0, //     amount_a_desired: i128,
+        &amount_1, //     amount_b_desired: i128,
+        &0, //     amount_a_min: i128,
+        &0 , //     amount_b_min: i128,
+        &test.user, //     to: Address,
+        &desired_deadline//     deadline: u64,
+    );
+}
+
+#[test]
+fn test_remove_liquidity() {
+    let test = SoroswapRouterTest::setup();
+    test.contract.initialize(&test.factory.address);
+    add_liquidity(&test);
+
+    let ledger_timestamp = 200;
+    let desired_deadline = 1000;
+    assert!(desired_deadline > ledger_timestamp);
+    test.env.ledger().with_mut(|li| {
+        li.timestamp = ledger_timestamp;
+    });
+
+    // TODO: Get rid of this hack?
+    test.env.budget().reset_unlimited();
+
+    let initial_user_balance: i128 = 10_000_000_000_000_000_000;
+    let expected_liquidity: i128 = 2_000_000_000_000_000_000;
+    static MINIMUM_LIQUIDITY: i128 = 1000;
+
+    test.contract.remove_liquidity(
+        &test.token_0.address, //     token_a: Address,
+        &test.token_1.address, //     token_b: Address,
+        &expected_liquidity.checked_sub(MINIMUM_LIQUIDITY).unwrap(), //     liquidity: i128,
+        &0, //     amount_a_min: i128,
+        &0 , //     amount_b_min: i128,
+        &test.user, //     to: Address,
+        &desired_deadline//     deadline: u64,
+    );
+   
+   // TODO: Test events
+
+   // Check that not, the user does not have any LP anymore
+   let pair_address = test.factory.get_pair(&test.token_0.address, &test.token_1.address);
+   let pair_client = SoroswapPairClient::new(&test.env, &pair_address);
+   assert_eq!(pair_client.balance(&test.user), 0);
+
+   // Check new user token balances, minus the minimum liquiity
+   assert_eq!(test.token_0.balance(&test.user), initial_user_balance.checked_sub(500).unwrap());
+   assert_eq!(test.token_1.balance(&test.user), initial_user_balance.checked_sub(2000).unwrap());
+   assert_eq!(test.token_0.balance(&pair_address), 500);
+   assert_eq!(test.token_1.balance(&pair_address), 2000);
+    
+}
