59319f1b1eed9eec8c0501e46c4bed0a7fd7e3b9
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-10-24 15:00:33 -0300

    move add_liquidity function to add_liquidity module

--- contracts/router/src/test/add_liquidity.rs
+++ contracts/router/src/test/add_liquidity.rs
@@ -193,3 +193,30 @@ fn test_add_liquidity() {
         &desired_deadline//     deadline: u64,
     );
 }
+
+
+// Pub function that will be used in other tests:
+
+
+pub fn add_liquidity(test: &SoroswapRouterTest){
+    let ledger_timestamp = 100;
+    let desired_deadline = 1000;
+    assert!(desired_deadline > ledger_timestamp);
+    test.env.ledger().with_mut(|li| {
+        li.timestamp = ledger_timestamp;
+    });
+
+    let amount_0: i128 = 1_000_000_000_000_000_000;
+    let amount_1: i128 = 4_000_000_000_000_000_000;
+
+    test.contract.add_liquidity(
+        &test.token_0.address, //     token_a: Address,
+        &test.token_1.address, //     token_b: Address,
+        &amount_0, //     amount_a_desired: i128,
+        &amount_1, //     amount_b_desired: i128,
+        &0, //     amount_a_min: i128,
+        &0 , //     amount_b_min: i128,
+        &test.user, //     to: Address,
+        &desired_deadline//     deadline: u64,
+    );
+}
