81a5f7ab82dee4288175b6b5d034968e47c65085
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-10-24 14:06:22 -0300

    move library function test into other file

---
+++ contracts/router/src/test/library_functions.rs
@@ -0,0 +1,4 @@
+
+    // Correct router.getAmountsOut after adding liquidity
+    // let path = vec![&test.env, test.token_0.address, test.token_1.address];
+    // assert_eq!(test.contract.router_get_amounts_out(&2, &path), vec![&test.env,2, 1]);
