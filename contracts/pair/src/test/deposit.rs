8a79f5ff5b8910eeffeb419a2074a8282fc8f48e
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-10-26 16:57:13 -0300

    change deposit to add_liquidity in test

--- contracts/pair/src/test/deposit.rs
+++ contracts/pair/src/test/deposit.rs
@@ -13,7 +13,7 @@ use soroban_sdk::{  symbol_short,
     
 // Pub function that will be used in other tests:
 
-pub fn deposit(test: &SoroswapPairTest, amount_0: &i128, amount_1: &i128){
+pub fn add_liquidity(test: &SoroswapPairTest, amount_0: &i128, amount_1: &i128){
     
     // User needs to send these tokens first to the contract
     test.token_0.transfer(&test.user, &test.contract.address, &amount_0);
@@ -31,12 +31,12 @@ fn deposit_basic() {
         li.timestamp = init_time;
     });
 
-    let original_0 = test.token_0.balance(&test.user);
-    let original_1 = test.token_0.balance(&test.user);
-    let amount_0 = 1_000_000_000_000_000_000;
-    let amount_1 = 4_000_000_000_000_000_000;
-    let expected_liquidity = 2_000_000_000_000_000_000;
-    let minimum_liquidity = 1_000;
+    let original_0: i128 = test.token_0.balance(&test.user);
+    let original_1: i128 = test.token_1.balance(&test.user);
+    let amount_0: i128 = 1_000_000_000_000_000_000;
+    let amount_1: i128 = 4_000_000_000_000_000_000;
+    let expected_liquidity: i128 = 2_000_000_000_000_000_000;
+    let minimum_liquidity: i128 = 1_000;
 
 
     // User needs to send these tokens first to the contract
@@ -54,8 +54,8 @@ fn deposit_basic() {
     test.contract.deposit(&test.user);
 
     // New balances:
-    assert_eq!(test.token_0.balance(&test.user), original_0 - amount_0);
-    assert_eq!(test.token_1.balance(&test.user), original_1 - amount_1);
+    assert_eq!(test.token_0.balance(&test.user), original_0.checked_sub(amount_0).unwrap());
+    assert_eq!(test.token_1.balance(&test.user), original_1.checked_sub(amount_1).unwrap());
 
     // New LP balance:
     assert_eq!(test.contract.my_balance(&test.user), expected_liquidity- minimum_liquidity);
@@ -70,5 +70,5 @@ fn deposit_basic_2() {
     let test = SoroswapPairTest::setup();
     let amount_0 = 1_000_000_000_000_000_000;
     let amount_1 = 4_000_000_000_000_000_000;
-    deposit(&test, &amount_0, &amount_1);
+    add_liquidity(&test, &amount_0, &amount_1);
 }
