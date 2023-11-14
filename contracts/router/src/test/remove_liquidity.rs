5d2c851dc571cee126a7295da9d0959b7f402919
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-11-14 19:16:11 -0300

    test other way

--- contracts/router/src/test/remove_liquidity.rs
+++ contracts/router/src/test/remove_liquidity.rs
@@ -413,3 +413,77 @@ fn test_remove_liquidity_inequal_amount_exact_minimum_out() {
    assert_eq!(test.token_1.balance(&pair_address), locked_1);
     
 }
+
+
+
+
+#[test]
+fn test_remove_liquidity_inequal_amount_exact_minimum_out_other_way() {
+    let test = SoroswapRouterTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize(&test.factory.address);
+    
+    let initial_user_balance = 10000000000000000000;
+    assert_eq!(test.token_0.balance(&test.user), initial_user_balance);
+    assert_eq!(test.token_1.balance(&test.user), initial_user_balance);
+
+    let amount_0: i128 = 4_000_000_000_000_000;
+    let amount_1: i128 = 999_000_000_000_000;
+
+    add_liquidity(&test, &amount_0, &amount_1);
+    let pair_address = test.factory.get_pair(&test.token_0.address, &test.token_1.address);
+
+    let     pair_client = SoroswapPairClient::new(&test.env, &pair_address);
+    // Check new balances:
+    assert_eq!(test.token_0.balance(&test.user), initial_user_balance.checked_sub(amount_0).unwrap());
+    assert_eq!(test.token_1.balance(&test.user), initial_user_balance.checked_sub(amount_1).unwrap());
+    assert_eq!(test.token_0.balance(&pair_address), amount_0);
+    assert_eq!(test.token_1.balance(&pair_address), amount_1);
+    // Check LP token balance
+    let expected_total_liquidity: i128 = (amount_0 * amount_1).sqrt(); // sqrt(amount_0, amount_1);
+    static MINIMUM_LIQUIDITY: i128 = 1000;
+    let expected_liquidity: i128 = expected_total_liquidity.checked_sub(MINIMUM_LIQUIDITY).unwrap();
+
+    assert_eq!(pair_client.balance(&test.user), expected_liquidity);
+    assert_eq!(pair_client.total_shares(), expected_total_liquidity);
+
+
+    let ledger_timestamp = 200;
+    let desired_deadline = 1000;
+    assert!(desired_deadline > ledger_timestamp);
+    test.env.ledger().with_mut(|li| {
+        li.timestamp = ledger_timestamp;
+    });
+
+    // TODO: Get rid of this hack?
+    test.env.budget().reset_unlimited();
+    let expected_to_remove_0 = (amount_0*expected_liquidity) / expected_total_liquidity;
+    let expected_to_remove_1 = (amount_1*expected_liquidity) / expected_total_liquidity;
+
+    let (removed_1, removed_0) = test.contract.remove_liquidity(
+        &test.token_1.address, //     token_b: Address,
+        &test.token_0.address, //     token_a: Address,
+        &expected_liquidity, //     liquidity: i128,
+        &expected_to_remove_1 , //     amount_b_min: i128,
+        &expected_to_remove_0, //     amount_a_min: i128,
+        &test.user, //     to: Address,
+        &desired_deadline//     deadline: u64,
+    );
+   
+    assert_eq!(removed_0, expected_to_remove_0);
+    assert_eq!(removed_1, expected_to_remove_1);
+    let locked_0 = amount_0- removed_0;
+    let locked_1 = amount_1- removed_1;
+    
+   // TODO: Test events
+
+   // Check that not, the user does not have any LP anymore
+   assert_eq!(pair_client.balance(&test.user), 0);
+
+   // Check new user token balances, minus the minimum liquiity
+   assert_eq!(test.token_0.balance(&test.user), initial_user_balance.checked_sub(locked_0).unwrap());
+   assert_eq!(test.token_1.balance(&test.user), initial_user_balance.checked_sub(locked_1).unwrap());
+   assert_eq!(test.token_0.balance(&pair_address), locked_0);
+   assert_eq!(test.token_1.balance(&pair_address), locked_1);
+    
+}
