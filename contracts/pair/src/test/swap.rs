5ec5dba3704e678d4602d65c933ae053f81854a0
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-11-23 19:17:30 -0300

    swap testing errors

--- contracts/pair/src/test/swap.rs
+++ contracts/pair/src/test/swap.rs
@@ -2,10 +2,138 @@ use crate::test::deposit::add_liquidity;
 use crate::test::{SoroswapPairTest};
 use soroban_sdk::{testutils::{Ledger}};
 
-#[test]
-fn swap() {
-    let test = SoroswapPairTest::setup();
     
+#[test]
+#[should_panic(expected = "SoroswapPair: not yet initialized")]
+fn swap_not_yet_initialized() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.swap(&0, &0, &test.user);
+}
+
+    
+#[test]
+#[should_panic(expected = "SoroswapPair: insufficient output amount")]
+fn swap_amounts_zero() {
+    let test = SoroswapPairTest::setup();    
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    test.contract.swap(&0, &0, &test.user);
+}
+
+    
+#[test]
+#[should_panic(expected = "SoroswapPair: negatives dont supported")]
+fn swap_amount_0_negative() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    test.contract.swap(&-1, &1, &test.user);
+}
+
+#[test]
+#[should_panic(expected = "SoroswapPair: negatives dont supported")]
+fn swap_amount_1_negative() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    test.contract.swap(&1, &-1, &test.user);
+}
+
+#[test]
+#[should_panic(expected = "SoroswapPair: insufficient liquidity")]
+fn swap_no_liquidity() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    test.contract.swap(&1, &1, &test.user);
+}
+
+
+#[test]
+#[should_panic(expected = "SoroswapPair: invalid to")]
+fn swap_to_token_0() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    add_liquidity(&test, &amount_0, &amount_1);
+    test.contract.swap(&1000, &0, &test.token_0.address);
+}
+
+
+#[test]
+#[should_panic(expected = "SoroswapPair: invalid to")]
+fn swap_to_token_1() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    add_liquidity(&test, &amount_0, &amount_1);
+    test.contract.swap(&1000, &0, &test.token_1.address);
+}
+
+
+#[test]
+#[should_panic(expected = "SoroswapPair: insufficient input amount")]
+fn swap_token_0_insufficient_input() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    add_liquidity(&test, &amount_0, &amount_1);
+    test.contract.swap(&1000, &0, &test.user);
+}
+
+
+#[test]
+#[should_panic(expected = "SoroswapPair: insufficient input amount")]
+fn swap_token_1_insufficient_input() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    add_liquidity(&test, &amount_0, &amount_1);
+    test.contract.swap(&0, &1000, &test.user);
+}
+
+
+#[test]
+#[should_panic(expected = "SoroswapPair: K constant is not met")]
+fn swap_token_0_low_sent() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    add_liquidity(&test, &amount_0, &amount_1);
+    test.token_0.transfer(&test.user, &test.contract.address, &1);
+    test.contract.swap(&0, &1000, &test.user);
+}
+
+
+#[test]
+#[should_panic(expected = "SoroswapPair: K constant is not met")]
+fn swap_token_1_low_sent() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    add_liquidity(&test, &amount_0, &amount_1);
+    test.token_1.transfer(&test.user, &test.contract.address, &1);
+    test.contract.swap(&1000, &0, &test.user);
+}
+
+
+
+#[test]
+fn swap_token_0() {
+    let test = SoroswapPairTest::setup();
     // TODO: Get rid of this hack?
     test.env.budget().reset_unlimited();
     
@@ -28,7 +156,6 @@ fn swap() {
     // The user sends the token first:
     test.token_0.transfer(&test.user, &test.contract.address, &swap_amount_0);
 
-    //    fn swap(e: Env, amount_0_out: i128, amount_1_out: i128, to: Address) {
     test.contract.swap(&0, &expected_output_amount_1, &test.user);
     
     assert_eq!(test.contract.get_reserves(),
@@ -37,9 +164,72 @@ fn swap() {
 
     assert_eq!(test.token_0.balance(&test.contract.address), amount_0.checked_add(swap_amount_0).unwrap());
     assert_eq!(test.token_1.balance(&test.contract.address), amount_1.checked_sub(expected_output_amount_1).unwrap());
-
-    // New balances:
     assert_eq!(test.token_0.balance(&test.user), original_0.checked_sub(amount_0).unwrap().checked_sub(swap_amount_0).unwrap());
     assert_eq!(test.token_1.balance(&test.user), original_1.checked_sub(amount_1).unwrap().checked_add(expected_output_amount_1).unwrap());
 
+}
+
+
+#[test]
+fn swap_token_1() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    
+    let original_0: i128 = test.token_0.balance(&test.user);
+    let original_1: i128 = test.token_1.balance(&test.user);
+
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    add_liquidity(&test, &amount_0, &amount_1);
+    let init_time = 12345;
+    test.env.ledger().with_mut(|li| {
+        li.timestamp = init_time;
+    });
+
+    let swap_amount_1: i128 = 10_000_000;
+    let expected_output_amount_0: i128 = 4533054;
+
+    // The user sends the token first:
+    test.token_1.transfer(&test.user, &test.contract.address, &swap_amount_1);
+
+    test.contract.swap(&expected_output_amount_0, &0, &test.user);
+    
+    assert_eq!(test.contract.get_reserves(),
+        (amount_0.checked_sub(expected_output_amount_0).unwrap(),
+        amount_1.checked_add(swap_amount_1).unwrap(),init_time));
+
+    assert_eq!(test.token_0.balance(&test.contract.address), amount_0.checked_sub(expected_output_amount_0).unwrap());
+    assert_eq!(test.token_1.balance(&test.contract.address), amount_1.checked_add(swap_amount_1).unwrap());
+    assert_eq!(test.token_0.balance(&test.user), original_0.checked_sub(amount_0).unwrap().checked_add(expected_output_amount_0).unwrap());
+    assert_eq!(test.token_1.balance(&test.user), original_1.checked_sub(amount_1).unwrap().checked_sub(swap_amount_1).unwrap());
+
+}
+
+
+#[test]
+#[should_panic(expected = "SoroswapPair: K constant is not met")]
+fn swap_token_1_optimal_plus_1() {
+    let test = SoroswapPairTest::setup();
+    test.env.budget().reset_unlimited();
+    
+    let original_0: i128 = test.token_0.balance(&test.user);
+    let original_1: i128 = test.token_1.balance(&test.user);
+
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+    add_liquidity(&test, &amount_0, &amount_1);
+    let init_time = 12345;
+    test.env.ledger().with_mut(|li| {
+        li.timestamp = init_time;
+    });
+
+    let swap_amount_1: i128 = 10_000_000;
+    let expected_output_amount_0: i128 = 4533054+1;
+
+    // The user sends the token first:
+    test.token_1.transfer(&test.user, &test.contract.address, &swap_amount_1);
+
+    test.contract.swap(&expected_output_amount_0, &0, &test.user);
 }
