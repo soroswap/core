fdd542cadb16c90d79c29bdefbfba4cba1765425
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-11-24 18:22:52 -0300

    test fee on when deposit/swap/deposit

--- contracts/pair/src/test/fee.rs
+++ contracts/pair/src/test/fee.rs
@@ -47,9 +47,9 @@ fn fee_off() {
 
 }
 
-
+// Testing fee when doing add_liquiquidity/swap/remove_liquidity
 #[test]
-fn fee_on() {
+fn fee_on_add_swap_remove() {
     let test = SoroswapPairTest::setup();    
     test.env.budget().reset_unlimited();
     test.factory.set_fees_enabled(&true);
@@ -172,4 +172,121 @@ fn fee_on() {
     assert_eq!(test.token_1.balance(&test.admin), expected_admin_out_token_1);
  
     
+}
+
+
+// Testing fee when doing add_liquiquidity/swap/add_liquidity
+#[test]
+fn fee_on_add_swap_add() {
+    let test = SoroswapPairTest::setup();    
+    test.env.budget().reset_unlimited();
+    test.factory.set_fees_enabled(&true);
+    assert_eq!(test.factory.fees_enabled(), true);
+    assert_eq!(test.factory.fee_to(), test.admin);
+    test.contract.initialize_pair(&test.factory.address, &test.token_0.address, &test.token_1.address);
+
+    let amount_0: i128 = 50_000_000;
+    let amount_1: i128 = 100_000_000;
+    let minimum_liquidity: i128 = 1_000;
+    let expected_liquidity: i128 =  70_710_678;
+    let original_0: i128 = test.token_0.balance(&test.user);
+    let original_1: i128 = test.token_1.balance(&test.user);
+
+    // ***************** DEPOSIT *****************
+    assert_eq!(test.contract.k_last(), 0);
+    add_liquidity(&test, &amount_0, &amount_1);
+
+    // If we deposit with fee on, we should see a change in the klast paramenter
+    //klast should be the new reserves (amount0 and amount1)
+    assert_eq!(test.contract.k_last(), amount_0.checked_mul(amount_1).unwrap());
+    assert_eq!(test.contract.total_shares(), expected_liquidity);
+    assert_eq!(test.token_0.balance(&test.user), original_0.checked_sub(amount_0).unwrap());
+    assert_eq!(test.token_1.balance(&test.user), original_1.checked_sub(amount_1).unwrap());
+
+    // ***************** SWAP *****************
+
+    let swap_amount_0 = 10_000_000;
+    // Amount does not changes... only the fee is splitted differently
+    let expected_output_amount_1 = 16624979;
+
+    test.token_0.transfer(&test.user, &test.contract.address, &swap_amount_0);
+    test.contract.swap(&0, &expected_output_amount_1, &test.user);
+    //klast does not gets updated in swaps
+    assert_eq!(test.contract.k_last(), amount_0.checked_mul(amount_1).unwrap());
+    
+    let new_expected_reserve_0= 60000000; //amount_0+swap_amount_0
+    assert_eq!(new_expected_reserve_0, amount_0+swap_amount_0);
+
+    let new_expected_reserve_1= 83375021; // amount_1-expected_output_amount_1; // 83375021
+    assert_eq!(new_expected_reserve_1, amount_1-expected_output_amount_1);
+
+    assert_eq!(test.contract.get_reserves(), (new_expected_reserve_0,new_expected_reserve_1,0));
+    assert_eq!(test.token_0.balance(&test.user), original_0-amount_0-swap_amount_0);
+    assert_eq!(test.token_1.balance(&test.user), original_1-amount_1+expected_output_amount_1);
+    
+    let k2_root=70728362; // new_expected_reserve_0.checked_mul(new_expected_reserve_1).unwrap().sqrt();
+    assert_eq!(new_expected_reserve_0.checked_mul(new_expected_reserve_1).unwrap().sqrt(), k2_root);
+
+    let k1_root = 70_710_678; // amount_0.checked_mul(amount_1).unwrap().sqrt();
+    assert_eq!(amount_0.checked_mul(amount_1).unwrap().sqrt(), k1_root);
+
+    // After the swap, k2 should be greater than k1
+    assert_eq!(k2_root > k1_root, true);
+
+
+
+    // ***************** DEPOSIT AGAIN! *****************
+    assert_eq!(test.contract.total_shares(), expected_liquidity);
+    assert_eq!(test.contract.get_reserves(), (new_expected_reserve_0,new_expected_reserve_1,0));
+    let new_amount_0: i128 = 1_000_000;
+    let new_amount_1: i128 = 1389583; //(new_amount_0*new_expected_reserve_1)/new_expected_reserve_0);
+    assert_eq!(new_amount_1, (new_amount_0*new_expected_reserve_1)/new_expected_reserve_0);
+    let new_got_liquidity = add_liquidity(&test, &new_amount_0, &new_amount_1);
+    assert_eq!(test.token_0.balance(&test.user), original_0-amount_0-swap_amount_0-new_amount_0);
+    assert_eq!(test.token_1.balance(&test.user), original_1-amount_1+expected_output_amount_1-new_amount_1);
+
+    assert_eq!(test.contract.k_last(), (new_expected_reserve_0+new_amount_0).checked_mul(new_expected_reserve_1+new_amount_1).unwrap());
+    
+    // We have the new liquidity minted to the admin = n
+    // n = expected_liquidity*(k2_root-k1_root)/(5k2_root + k1_root)
+    // = 2946,719213655 --> 2946
+    let n = 2946;
+    let numerator = expected_liquidity.checked_mul(k2_root-k1_root).unwrap(); //1250447629752
+    assert_eq!(numerator, 1250447629752);
+    let denominator = (5_i128).checked_mul(k2_root).unwrap().checked_add(k1_root).unwrap();
+    assert_eq!(denominator, 424352488);
+    assert_eq!(n, numerator/denominator);
+    assert_eq!(numerator.checked_div(denominator).unwrap(), n);
+
+    // We have the new liquidity minted to the user:
+    /*
+        lets check new LP balance:
+
+        let shares_a = (amount_0.checked_mul(total_shares).unwrap()).checked_div(reserve_0).unwrap();
+        let shares_b = (amount_1.checked_mul(total_shares).unwrap()).checked_div(reserve_1).unwrap();
+        shares_a.min(shares_b)
+    */
+
+    // shares_0 = (1000000 * (70710678+2946)) / 60000000 = 1178560
+    let shares_0 = 1178560; //new_amount_0 * (expected_liquidity+n) / new_expected_reserve_0;
+    assert_eq!(shares_0, (new_amount_0 * (expected_liquidity+n)) / new_expected_reserve_0);
+
+
+    // (1389583 * (70710678+2946)) / 83375021 = 1178559
+    let shares_1 = 1178559; //new_amount_1 * (expected_liquidity+n) / new_expected_reserve_1;
+    assert_eq!(shares_1, (new_amount_1 * (expected_liquidity+n)) / new_expected_reserve_1);
+
+    // min (shares_0, shares_1) = 1178559;
+    let expected_minted_liquidity = 1178559;
+    assert_eq!(new_got_liquidity, expected_minted_liquidity);
+
+
+    
+    // whe should have minted n shares to the admin:
+    assert_eq!(test.contract.total_shares(), expected_liquidity+n+expected_minted_liquidity);
+    assert_eq!(test.contract.my_balance(&test.contract.address), minimum_liquidity);
+    assert_eq!(test.contract.my_balance(&test.admin), n);
+    assert_eq!(test.contract.my_balance(&test.user), expected_minted_liquidity+ (expected_liquidity-minimum_liquidity));
+
+
 }
