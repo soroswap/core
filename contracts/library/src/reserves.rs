79d92c5f0ba511cef43c4510ec45e3555d06fcfb
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-12-11 15:22:12 +0200

    use latest soroswap pair wasm

--- contracts/library/src/reserves.rs
+++ contracts/library/src/reserves.rs
@@ -4,7 +4,7 @@ use crate::error::LibraryError;
 
 mod pair {
     soroban_sdk::contractimport!(
-        file = "../pair/target/wasm32-unknown-unknown/release/soroswap_pair.wasm"
+        file = "./src/soroswap_pair.wasm"
     );
 }
 use pair::Client as SoroswapPairClient;
