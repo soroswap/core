361b7f5b5b860ba25475727fccdeec716fa3b9ed
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-12-11 20:51:56 +0200

    router is 400 and combined is 500

--- contracts/router/src/error.rs
+++ contracts/router/src/error.rs
@@ -7,31 +7,31 @@ use soroswap_library::{SoroswapLibraryError};
 #[repr(u32)]
 pub enum SoroswapRouterError {
     /// SoroswapRouter: not yet initialized
-    NotInitialized = 1,
+    NotInitialized = 401,
 
     /// SoroswapRouter: negative amount is not allowed
-    NegativeNotAllowed = 2,
+    NegativeNotAllowed = 402,
 
     /// SoroswapRouter: deadline expired
-    DeadlineExpired = 3,
+    DeadlineExpired = 403,
     
     /// SoroswapRouter: already initialized
-    InitializeAlreadyInitialized = 4,
+    InitializeAlreadyInitialized = 404,
 
     /// SoroswapRouter: insufficient a amount
-    InsufficientAAmount = 5,
+    InsufficientAAmount = 405,
 
     /// SoroswapRouter: insufficient b amount
-    InsufficientBAmount = 6,
+    InsufficientBAmount = 406,
 
     /// SoroswapRouter: insufficient output amount
-    InsufficientOutputAmount = 7,
+    InsufficientOutputAmount = 407,
 
     /// SoroswapRouter: excessive input amount
-    ExcessiveInputAmount = 8,
+    ExcessiveInputAmount = 408,
 
     /// SoroswapRouter: pair does not exist
-    PairDoesNotExist = 9,
+    PairDoesNotExist = 409,
 
 }
 
@@ -41,22 +41,22 @@ pub enum SoroswapRouterError {
 #[repr(u32)]
 // Define a new set of integer literals for the CombinedError enum
 pub enum CombinedRouterError {
-    RouterNotInitialized = 101,
-    RouterNegativeNotAllowed = 102,
-    RouterDeadlineExpired = 103,
-    RouterInitializeAlreadyInitialized = 104,
-    RouterInsufficientAAmount = 105,
-    RouterInsufficientBAmount = 106,
-    RouterInsufficientOutputAmount = 107,
-    RouterExcessiveInputAmount = 108,
-    RouterPairDoesNotExist = 109,
+    RouterNotInitialized = 501,
+    RouterNegativeNotAllowed = 502,
+    RouterDeadlineExpired = 503,
+    RouterInitializeAlreadyInitialized = 504,
+    RouterInsufficientAAmount = 505,
+    RouterInsufficientBAmount = 506,
+    RouterInsufficientOutputAmount = 507,
+    RouterExcessiveInputAmount = 508,
+    RouterPairDoesNotExist = 509,
 
-    LibraryInsufficientAmount = 110,
-    LibraryInsufficientLiquidity = 111,
-    LibraryInsufficientInputAmount = 112,
-    LibraryInsufficientOutputAmount = 113,
-    LibraryInvalidPath = 114,
-    LibrarySortIdenticalTokens = 115,
+    LibraryInsufficientAmount = 510,
+    LibraryInsufficientLiquidity = 511,
+    LibraryInsufficientInputAmount = 512,
+    LibraryInsufficientOutputAmount = 513,
+    LibraryInvalidPath = 514,
+    LibrarySortIdenticalTokens = 515,
 }
 
 impl From<SoroswapLibraryError> for CombinedRouterError {
