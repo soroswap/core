e0208a67c3d2c9fbd1c3fb9623d046df9c174d4a
Matias Poblete <matias.pobleteduran@gmail.com>
2023-12-29 16:45:26 -0300

    Added liquidity diff to result tables and code cleanup

--- scripts/manual_testing/remove_liquidity.sh
+++ scripts/manual_testing/remove_liquidity.sh
@@ -214,8 +214,8 @@ soroban contract invoke \
   my_balance \
   --id "$USER_PUBLIC"
 
-printTokensBalanceDiff "Remove_liquidity" $TOKEN_0_SYMBOL $TOKEN_0_ADDRESS $TOKEN_0_FIRST_BALANCE $TOKEN_1_SYMBOL $TOKEN_1_ADDRESS $TOKEN_1_FIRST_BALANCE
-printLPTable $PAIR_ADDRESS
+printTokensBalanceDiff "Remove_liquidity" $TOKEN_0_SYMBOL $TOKEN_0_ADDRESS $TOKEN_0_FIRST_BALANCE $TOKEN_1_SYMBOL $TOKEN_1_ADDRESS $TOKEN_1_FIRST_BALANCE "LP Balance" $PAIR_ADDRESS $LP_BALANCE
+
 
 
 
