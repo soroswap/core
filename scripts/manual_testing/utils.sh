4cfdb35e4010af4769e7e6ecd74c55c41b704aac
Matias Poblete <matias.pobleteduran@gmail.com>
2024-01-09 15:12:37 -0300

    Added docs to functions

--- scripts/manual_testing/utils.sh
+++ scripts/manual_testing/utils.sh
@@ -19,6 +19,11 @@ USER_PUBLIC=$(cat /workspace/.soroban/user_public)
 TOKENS_FILE="/workspace/.soroban/tokens.json"
 USER_SECRET=$(cat /workspace/.soroban/user_secret)
 
+# Function to get the balance of a token for a given token address
+# Parameters:
+#   - tokenAddress: The address of the token
+# Returns:
+#   - The token balance as an integer
 getTokenBalance() {
     local tokenAddress="$1"
     local TOKEN_BALANCE="$(soroban contract invoke \
@@ -33,6 +38,13 @@ getTokenBalance() {
 
 TOKEN_0_ADDRESS=$(jq -r --arg NETWORK "$NETWORK" '.[] | select(.network == $NETWORK) | .tokens[2].address' "$TOKENS_FILE")
 TOKEN_0_SYMBOL=$(jq -r --arg NETWORK "$NETWORK" '.[] | select(.network == $NETWORK) | .tokens[2].symbol' "$TOKENS_FILE")
+
+
+
+# Function to print a table of tokens and their balances
+# Arguments:
+#   - List of token names
+#   - List of token balances
 
 #to make it work, we need to pass the token symbol and the token address in that specific order
 #printTokensTable $TOKEN_0_SYMBOL $TOKEN_0_ADDRESS $TOKEN_1_SYMBOL $TOKEN_1_ADDRESS ...
@@ -60,45 +72,48 @@ printTokensTable() {
     echo ""
 }
 
-#Recives (operation: ['add_liquidity', 'remove_liquidity', 'swap']) (token_symbol) (token_address) (token_balance_before)
+# Function: printTokensBalanceDiff
+# Description: Prints the difference in token balances before and after a specified operation.
+# Parameters:
+#   - operation: The operation performed ('add_liquidity', 'remove_liquidity', 'swap').
+#   - token_symbol: The symbol of the token.
+#   - token_address: The address of the token.
+#   - token_balance_before: The token balance before the operation.
+# Returns: None
+
 printTokensBalanceDiff(){
-    local count=0
-    local token_symbol=""
-    local token_balance_before=""
-    local token_balance_after=""
-    local delta_balance=""
-    echo ""
-    display_colored_text BLUE " ------------------------------------------------------------------------------------- "
-    printf "\033[1;44;30m |  %-10s |  %-10s  |  %-15s  | %-27s | \033[0m\n" "Token Name" "Initial Balance" "Actual Balance" "Diff after $1"
-    display_colored_text BLUE " ------------------------------------------------------------------------------------- "
-    for ((i=2; i<=$#; i++)); do
-      case $count in
-        0)
-            token_symbol="${!i}"
-            ;;
-        1)
-            token="${!i}"
-            token_balance_after=$(getTokenBalance "$token")
-            ;;
-        2)
-            token_balance_before="${!i}"
-            ;;
-      esac
-      ((count++))
-      if ((count % 3 == 0)); then
-        delta_balance=$(($token_balance_after - $token_balance_before))
-        if (($delta_balance > 1)); then
-            delta_balance="+$delta_balance"
-        fi
-        printf "\033[1;44;30m | %-10s  | %-16s  |  %-15s  | %-27s | \033[0m\n" "$token_symbol" "$token_balance_before" "$token_balance_after" "$delta_balance"
-        display_colored_text BLUE " ------------------------------------------------------------------------------------- " 
-        count=0
-      fi
-    done
+        local count=0
+        local token_symbol=""
+        local token_balance_before=""
+        local token_balance_after=""
+        local delta_balance=""
+        echo ""
+        display_colored_text BLUE " ------------------------------------------------------------------------------------- "
+        printf "\033[1;44;30m |  %-10s |  %-10s  |  %-15s  | %-27s | \033[0m\n" "Token Name" "Initial Balance" "Actual Balance" "Diff after $1"
+        display_colored_text BLUE " ------------------------------------------------------------------------------------- "
+        for ((i=2; i<=$#; i++)); do
+            case $count in
+                0)
+                        token_symbol="${!i}"
+                        ;;
+                1)
+                        token="${!i}"
+                        token_balance_after=$(getTokenBalance "$token")
+                        ;;
+                2)
+                        token_balance_before="${!i}"
+                        ;;
+            esac
+            ((count++))
+            if ((count % 3 == 0)); then
+                delta_balance=$(($token_balance_after - $token_balance_before))
+                if (($delta_balance > 1)); then
+                        delta_balance="+$delta_balance"
+                fi
+                printf "\033[1;44;30m | %-10s  | %-16s  |  %-15s  | %-27s | \033[0m\n" "$token_symbol" "$token_balance_before" "$token_balance_after" "$delta_balance"
+                display_colored_text BLUE " ------------------------------------------------------------------------------------- " 
+                count=0
+            fi
+        done
 }
 
-# Function to print yesterday's date
-printYesterdayDate() {
-    local yesterday=$(date -d "yesterday" +"%Y-%m-%d")
-    echo "Yesterday's date: $yesterday"
-}
