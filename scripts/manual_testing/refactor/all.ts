1d085189e5ffe0046d578f82efda9707d13fe4be
Matias Poblete <matias.pobleteduran@gmail.com>
2024-01-09 16:24:53 -0300

    Added network handling

--- scripts/manual_testing/refactor/all.ts
+++ scripts/manual_testing/refactor/all.ts
@@ -1,4 +1,3 @@
-import * as sdk from "stellar-sdk";
 import {getRouterContractAddress, colors} from "./utils/utils";
 import { TransactionBuilder } from "./utils/TransactionBuilder";
 import {
@@ -13,51 +12,77 @@ import {
 const args = process.argv.slice(2);
 
 // Access the individual arguments
-let network = args[0];
+/**
+ * Represents the network used for testing.
+ * @type {string}
+ */
+const network = args[0];
+
 const mode = args[1];
 
 console.log("network:", network)
 console.log("running on:", mode)
 let txMaker: TransactionBuilder;
-const networkPassphrase = network === "testnet" ?
-    sdk.Networks.TESTNET : network == "standalone" ?
-        sdk.Networks.STANDALONE :
-        sdk.Networks.PUBLIC;
 
 
-switch (mode) {
-    case "local":
-        console.log("Using deployed contracts from .soroban folder")
+
+const testAll = async (network: string, mode: string) => {
+    let apiUri: string;
+    let rpcUri: string;
+    let routerContractAddress: string;
+    switch (network) {
+        case "standalone":
+            console.log(colors.green, 'Using standalone network')
+            apiUri = "http://soroswapCoreApi:8010"
+            rpcUri = "http://stellar"
+            routerContractAddress = await getRouterContractAddress(apiUri, network)
+
+            txMaker = new TransactionBuilder(
+                `${rpcUri}:8000`,
+                `${rpcUri}:8000/soroban/rpc`,
+                `${rpcUri}:8000/friendbot?addr=`,
+                routerContractAddress,
+                network
+            );
         break;
-    case "public":
-        console.log("Using deployed contracts from /public folder")
-        break;
-    default:
-        console.log("Usage: local|public")
-        console.log("local: use contracts from the .soroban folder (local deployements)")
-        console.log("public: use contracts from the /public folder (addresses in production?)")
-        break;
-}
-
-const testAll = async () => {
-    let apiUri = "http://soroswapCoreApi:8010"
-    let rpcUri = "http://stellar"
-    let routerContractAddress = await getRouterContractAddress(apiUri, network)
-
-    txMaker = new TransactionBuilder(
-        `${rpcUri}:8000`,
-        `${rpcUri}:8000/soroban/rpc`,
-        `${rpcUri}:8000/friendbot?addr=`,
-        routerContractAddress,
-        network
-    );
+        case "testnet":
+            console.log(colors.green, 'Using testnet network')
+            apiUri = "https://horizon-testnet.stellar.org"
+            rpcUri = "http://stellar"
+            routerContractAddress = await getRouterContractAddress(apiUri, network)
+            txMaker = new TransactionBuilder(
+                `${rpcUri}`,
+                `${rpcUri}/soroban/rpc`,
+                "https://friendbot.stellar.org/?addr=",
+                routerContractAddress,
+                network
+            );
+            break;
+        default:
+            console.log(colors.yellow, "Please pass your selected network as an argument")
+            console.log(colors.yellow, "Usage: ts-node all.ts standalone|testnet")
+    }
+    switch (mode) {
+        case "local":
+            console.log("Using deployed contracts from .soroban folder")
+            break;
+        case "public":
+            console.log("Using deployed contracts from /public folder")
+            break;
+        default:
+            console.log("Usage: local|public")
+            console.log("local: use contracts from the .soroban folder (local deployements)")
+            console.log("public: use contracts from the /public folder (addresses in production)")
+            break;
+    }
+    
     await txMaker.getRouterContractAddress()
     await generateUsers();
-    await mint(txMaker);
+    await mint(txMaker, network);
     await add_liquidity(txMaker);
     await swap(txMaker);
     await remove_liquidity(txMaker);
 }
 
-testAll()
+testAll(network, mode)
 
