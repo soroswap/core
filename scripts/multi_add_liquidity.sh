91bbee42f2319c362d68fe812a0ff448557b78d6
Matias Poblete <matias.pobleteduran@gmail.com>
2024-01-16 11:18:07 -0300

    Renamed and re-styled script

--- scripts/multi_add_liquidity.sh
+++ scripts/multi_add_liquidity.sh
@@ -6,16 +6,21 @@ source /workspace/scripts/manual_testing/utils.sh
 echo "Using deployed contracts from .soroban folder"
 TOKENS_FILE="/workspace/.soroban/tokens.json"
 ROUTER_FILE="/workspace/.soroban/router.json"
-SOROBAN_TOKENS_FOLDER="/workspace/.soroban/soroban_tokens/"
-PAIRS_FILE="/workspace/.soroban/pairs.json"
-ROUTER_WASM="/workspace/contracts/router/target/wasm32-unknown-unknown/release/soroswap_router.optimized.wasm"
 SOROBAN_RPC_HOST="http://stellar:8000"
 FRIENDBOT_URL="$SOROBAN_RPC_HOST/friendbot"
+
+# Define constants for color-coded output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
 
 #Get the router contract address
 ROUTER_CONTRACT=$(jq -r --arg NETWORK "$NETWORK" '.[] | select(.network == $NETWORK) | .router_address' "$ROUTER_FILE")
 echo ""
-display_colored_text GREEN "ROUTER_CONTRACT: $ROUTER_CONTRACT"
+echo -e "${PURPLE}ROUTER_CONTRACT: $ROUTER_CONTRACT${NC}"
 
 # Get the token admin address
 TOKEN_ADMIN_ADDRESS="$(soroban config identity address token-admin)"
@@ -25,7 +30,7 @@ echo Fund tokenAdmin account from friendbot
 echo This will fail if the account already exists, but it\' still be fine.
 curl  -X POST "$FRIENDBOT_URL?addr=$TOKEN_ADMIN_ADDRESS"
 echo ""
-display_colored_text BLUE "funded $TOKEN_ADMIN_ADDRESS"
+echo -e "${GREEN}funded $TOKEN_ADMIN_ADDRESS${NC}"
 echo ""
 #Read the tokens array from the tokens.json file
 TOKENS=($(jq -r '.[].tokens[].address' "$TOKENS_FILE"))
@@ -33,33 +38,26 @@ TOKENS=($(jq -r '.[].tokens[].address' "$TOKENS_FILE"))
 #Get the tokens to use on this operation
 TOKEN_A=${TOKENS[6]} #XTAR
 TOKEN_F=${TOKENS[7]} #USDC
-
 TOKEN_B=${TOKENS[8]} #XRP
 TOKEN_C=${TOKENS[9]} #ARST
-TOKEN_D=${TOKENS[10]} #AQUA
 TOKEN_E=${TOKENS[11]} #EURC
 
 #Get the token A decimals 
 
-display_colored_text BLUE "TOKENS:"
+echo -e "${BLUE}Usign the following tokens:${NC}"
 echo "XTAR:" $TOKEN_A
 echo "XRP:" $TOKEN_B
 echo "ARST:" $TOKEN_C
-echo "AQUA:" $TOKEN_D
 echo "EURC:" $TOKEN_E
 echo "XTAR:" $TOKEN_F
 
 echo ""
 
-
-#Acceder a un valor espefifico del array
-#"${TOKENS[6]}"
-
 # Create an array with all tokens
-ALL_TOKENS=($TOKEN_A $TOKEN_B $TOKEN_C $TOKEN_D $TOKEN_E $TOKEN_F)
+ALL_TOKENS=($TOKEN_A $TOKEN_B $TOKEN_C $TOKEN_E $TOKEN_F)
 
 #Mint the tokens to the token admin account
-display_colored_text BLUE "Minting tokens to the token admin account"
+echo -e "${BLUE}Minting tokens to the token admin account${NC}"
 
 # Loop through each token and mint it
 for TOKEN in "${ALL_TOKENS[@]}"
@@ -85,30 +83,30 @@ PATH_2=($TOKEN_A $TOKEN_B $TOKEN_F)
 PATH_3=($TOKEN_A $TOKEN_C $TOKEN_E $TOKEN_F)
 
 echo ""
-display_colored_text BLUE "PATHS:"
+echo -e "${BLUE}Adding liquidity to the following pairs${NC}"
 echo ""
-display_colored_text YELLOW "---------------------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------------------------"
 
 for ((i=0; i<${#PATH_1[@]}-1; i++))
 do
     echo "${PATH_1[i]} and ${PATH_1[i+1]}"
 done
 
-display_colored_text YELLOW "---------------------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------------------------"
 
 for ((i=0; i<${#PATH_2[@]}-1; i++))
 do
     echo "${PATH_2[i]} and ${PATH_2[i+1]}"
 done
 
-display_colored_text YELLOW "---------------------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------------------------"
 
 for ((i=0; i<${#PATH_3[@]}-1; i++))
 do
     echo "${PATH_3[i]} and ${PATH_3[i+1]}"
 done
 
-display_colored_text YELLOW "---------------------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------------------------"
 
 #Define the constants for the add_liquidity function
 AMOUNT_A_MIN=0
@@ -119,11 +117,11 @@ DEADLINE=$(date -d "+1 hour" +%s)
 #--------------------------------------------------------------------------------------------------------------------------------------
 #Add liquidity to the pairs in path 1
 echo ""
-display_colored_text BLUE "Adding liquidity to path 1"
+echo -e "${BLUE}Adding liquidity to path 1${NC}"
 echo ""
 for ((i=0; i<${#PATH_1[@]}-1; i++))
 do
-    display_colored_text GREEN "Adding 20000 to ${TOKENS[i]} and 19500 ${TOKENS[i+1]}"
+    echo -e "${YELLOW}Adding 20000 to ${TOKENS[i]} and 19500 to ${TOKENS[i+1]}${NC}"
     soroban contract invoke \
         --network $NETWORK \
         --source token-admin \
@@ -147,11 +145,11 @@ AMOUNT_B_DESIRED_VALUES=(74000 70000)
 
 #Add liquidity to the pairs in path 2
 echo ""
-display_colored_text BLUE "Adding liquidity to path 2"
+echo -e "${BLUE}Adding liquidity to path 2${NC}"
 echo ""
 for ((i=0; i<${#PATH_2[@]}-1; i++))
 do
-    display_colored_text GREEN "Adding ${AMOUNT_A_DESIRED_VALUES[i]} to ${TOKENS[i]} and ${AMOUNT_B_DESIRED_VALUES[i]} ${TOKENS[i+1]}"
+    echo -e "${YELLOW}Adding ${AMOUNT_A_DESIRED_VALUES[i]} to ${TOKENS[i]} and ${AMOUNT_B_DESIRED_VALUES[i]} to ${TOKENS[i+1]}${NC}"
     soroban contract invoke \
         --network $NETWORK \
         --source token-admin \
@@ -174,11 +172,11 @@ AMOUNT_A_DESIRED_VALUES=(60000 55000 80000)
 AMOUNT_B_DESIRED_VALUES=(61000 53500 80500)
 #Add liquidity to the pairs in path 3
 echo ""
-display_colored_text BLUE "Adding liquidity to path 3"
+echo -e "${BLUE}Adding liquidity to path 3${NC}"
 echo ""
 for ((i=0; i<${#PATH_3[@]}-1; i++))
 do
-    display_colored_text GREEN "Adding ${AMOUNT_A_DESIRED_VALUES[i]} to ${TOKENS[i]} and ${AMOUNT_B_DESIRED_VALUES[i]} ${TOKENS[i+1]}"
+    echo -e "${YELLOW}Adding ${AMOUNT_A_DESIRED_VALUES[i]} to ${TOKENS[i]} and ${AMOUNT_B_DESIRED_VALUES[i]} to ${TOKENS[i+1]}${NC}"
     soroban contract invoke \
         --network $NETWORK \
         --source token-admin \
@@ -194,3 +192,6 @@ do
         --to "$TO" \
         --deadline "$DEADLINE"
 done
+
+echo ""
+echo -e "${GREEN}Liquidity added successfully.${NC}"
