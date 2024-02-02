6d28a08672ea7205bba1a456c8646e36475e596b
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-02-02 12:28:23 -0300

    feat(pair): soroban pair token also extends 30 days in instance and 60 days in persistance

--- contracts/pair/src/soroswap_pair_token/storage_types.rs
+++ contracts/pair/src/soroswap_pair_token/storage_types.rs
@@ -1,10 +1,10 @@
 use soroban_sdk::{contracttype, Address};
 
 pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
-pub(crate) const INSTANCE_BUMP_AMOUNT: u32 = 7 * DAY_IN_LEDGERS;
+pub(crate) const INSTANCE_BUMP_AMOUNT: u32 = 30 * DAY_IN_LEDGERS;
 pub(crate) const INSTANCE_LIFETIME_THRESHOLD: u32 = INSTANCE_BUMP_AMOUNT - DAY_IN_LEDGERS;
 
-pub(crate) const BALANCE_BUMP_AMOUNT: u32 = 30 * DAY_IN_LEDGERS;
+pub(crate) const BALANCE_BUMP_AMOUNT: u32 = 60 * DAY_IN_LEDGERS;
 pub(crate) const BALANCE_LIFETIME_THRESHOLD: u32 = BALANCE_BUMP_AMOUNT - DAY_IN_LEDGERS;
 
 #[derive(Clone)]
