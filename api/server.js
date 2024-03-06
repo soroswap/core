aa7f11f096d10d3966f4ec6b6d7c7189aa613d39
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-03-06 12:23:51 -0300

    fix: new contract address book

--- api/server.js
+++ api/server.js
@@ -10,12 +10,13 @@ const directory = isVercel
   ? path.join(__dirname, "..", "public")
   : "/workspace/.soroban";
 
+  
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(`healthy`);
 });
-
+  
 app.get("/api/tokens", (req, res) => {
   const tokensFile = path.join(directory, "tokens.json");
   if (fs.existsSync(tokensFile)) {
@@ -25,7 +26,7 @@ app.get("/api/tokens", (req, res) => {
 
   res.status(404).send({ error: "file not found" });
 });
-
+  
 app.get("/api/random_tokens", (req, res) => {
   const tokensFile = path.join(directory, "random_tokens.json");
   if (fs.existsSync(tokensFile)) {
@@ -35,51 +36,30 @@ app.get("/api/random_tokens", (req, res) => {
 
   res.status(404).send({ error: "file not found" });
 });
+  
+app.get("/api/:network/:contractName", (req, res) => {
+  const { network, contractName } = req.params;
+  const contractsFile = `${directory}/${network}.contracts.json`;
 
-app.get("/api/factory", (req, res) => {
-  const factoryFile = `${directory}/factory.json`;
+  if (fs.existsSync(contractsFile)) {
+    const contractsData = JSON.parse(fs.readFileSync(contractsFile, 'utf8'));
 
-  if (fs.existsSync(factoryFile)) {
-    res.set("Cache-Control", "no-store");
-    return res.sendFile(factoryFile);
+    // Check if the contract name exists in the "ids" section
+    if (contractsData.ids && contractsData.ids[contractName]) {
+      res.set("Cache-Control", "no-store");
+      // Return the contract address
+      return res.send({ address: contractsData.ids[contractName] });
+    } else {
+      // Contract name not found in the file
+      return res.status(404).send({ error: "contract name not found" });
+    }
   }
-
-  res.status(404).send({ error: "file not found" });
-});
-
-app.get("/api/keys", (req, res) => {
-  const keysFile = `${directory}/token_admin_keys.json`;
-
-  if (fs.existsSync(keysFile)) {
-    res.set("Cache-Control", "no-store");
-    return res.sendFile(keysFile);
-  }
-
-  res.status(404).send({ error: "file not found" });
-});
-
-app.get("/api/pairs", (req, res) => {
-  const tokensFile = `${directory}/pairs.json`;
-
-  if (fs.existsSync(tokensFile)) {
-    res.set("Cache-Control", "no-store");
-    return res.sendFile(tokensFile);
-  }
-
-  res.status(404).send({ error: "file not found" });
-});
-
-app.get("/api/router", (req, res) => {
-  const routerFile = `${directory}/router.json`;
-
-  if (fs.existsSync(routerFile)) {
-    res.set("Cache-Control", "no-store");
-    return res.sendFile(routerFile);
-  }
-
-  res.status(404).send({ error: "file not found" });
-});
-
+  
+    // File not found
+    res.status(404).send({ error: "file not found" });
+  });
+  
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+  
