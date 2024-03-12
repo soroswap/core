0d9bcfbf7d569f060e82179da00094f37b4a10a9
Benjamin Guiloff <byguiloff@uc.cl>
2024-03-12 11:02:25 -0300

    update script

--- scripts/set_trustlines.ts
+++ scripts/set_trustlines.ts
@@ -16,12 +16,16 @@ export interface tokensResponse {
 }
 
 export async function setTrustline(tokenSymbol: string, tokenIssuer: string) {
+  console.log('Public key: ', loadedConfig.admin.publicKey());
+  console.log('Horizon RPC', loadedConfig.horizonRpc);
   let source = await loadedConfig.horizonRpc.loadAccount(loadedConfig.admin.publicKey()!);
+  console.log('Source: ', source);
 
   const operation = StellarSdk.Operation.changeTrust({
     source: source.accountId(),
     asset: new StellarSdk.Asset(tokenSymbol, tokenIssuer),
   });
+  console.log('Operation: ', operation);
 
   const txn = new StellarSdk.TransactionBuilder(source, {
     fee: '100',
@@ -32,7 +36,11 @@ export async function setTrustline(tokenSymbol: string, tokenIssuer: string) {
     .setTimeout(StellarSdk.TimeoutInfinite)
     .build();
 
+  console.log('Transaction: ', txn);
+
   txn.sign(loadedConfig.admin);
+
+  console.log('\nTransaction Signed: ', txn);
 
   try {
     let response = await loadedConfig.horizonRpc.submitTransaction(txn);
@@ -48,7 +56,7 @@ export async function setTrustlines() {
     const { data } = await axios.get(
       'https://raw.githubusercontent.com/soroswap/token-list/main/tokenList.json'
     );
-    tokensList = data;
+    tokensList = data.tokens;
   } else {
     const axiosInstance = axios.create({
       baseURL: 'https://api.soroswap.finance',
@@ -57,12 +65,10 @@ export async function setTrustlines() {
       },
     });
     const { data } = await axiosInstance.get('/api/tokens');
-    tokensList = data;
+    tokensList = data.find((item: tokensResponse) => item.network === network).tokens;
   }
 
-  const networkTokens = tokensList.find((item: tokensResponse) => item.network === network);
-
-  for (const token of networkTokens.tokens) {
+  for (const token of tokensList) {
     if (!token.issuer) {
       console.log(`No issuer found for ${token.code}`);
     } else {
